5735c2b0f000d51233e79f6672aeea2938ef5ac2
rtx-vm <123107610+rtx-vm@users.noreply.github.com>
2023-02-06 17:44:38 +0000

    rtv v1.5.2

--- static/install.sh
+++ static/install.sh
@@ -55,10 +55,10 @@ get_checksum() {
 	os="$(get_os)"
 	arch="$(get_arch)"
 
-	checksum_linux_x86_64="009d260a937f6267fe6e5c0a3dcd8fe344c94527a160596926dad7cb36470e73  ./rtx-v1.5.1-linux-x64.tar.gz"
-	checksum_linux_arm64="1a8491038447e88692820c487e331a520ceba2e4cf834970dbe20d215ec44b25  ./rtx-v1.5.1-linux-arm64.tar.gz"
-	checksum_macos_x86_64="639e33f6403601f915ecaaa8aa21330427ca6870a95d74a243b3aa76d580b66a  ./rtx-v1.5.1-macos-x64.tar.gz"
-	checksum_macos_arm64="fbdb85f8cc432feb25bc3096ad5721c242b9fffb9d05f0b48f87d31fd56e8dae  ./rtx-v1.5.1-macos-arm64.tar.gz"
+	checksum_linux_x86_64="f2d9a6a32a9f9412ae83e84eea4e1cc283f17ed4066803b8677be158f9204ca8  ./rtx-v1.5.2-linux-x64.tar.gz"
+	checksum_linux_arm64="31b3168286ac226ecc61491a319d36a38cc3cac1b412e86f0a6aafb60fdea287  ./rtx-v1.5.2-linux-arm64.tar.gz"
+	checksum_macos_x86_64="1238f02e73bef8001fefff97632417fa1ba532d7cafdd920ee56642901b0abc1  ./rtx-v1.5.2-macos-x64.tar.gz"
+	checksum_macos_arm64="67ddc3d5b112a8e73b97332192321b8f219640626cdd81be2e0a3ac45fac64d4  ./rtx-v1.5.2-macos-arm64.tar.gz"
 
 	if [ "$os" = "linux" ] && [ "$arch" = "x64" ]; then
 		echo "$checksum_linux_x86_64"
@@ -101,7 +101,7 @@ download_file() {
 
 install_rtx() {
   # download the tarball
-  version="v1.5.1"
+  version="v1.5.2"
   os="$(get_os)"
   arch="$(get_arch)"
   xdg_data_home="${XDG_DATA_HOME:-$HOME/.local/share}"
